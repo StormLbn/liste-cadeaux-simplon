104386811559fd040910980dd4a5f21ad0d5984e
ManuelVali77 <manuelv@outlook.fr>
2023-01-30 14:52:40 +0100

    composent header + barre de recherche

--- IDKDO/src/app/app.module.ts
+++ IDKDO/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
