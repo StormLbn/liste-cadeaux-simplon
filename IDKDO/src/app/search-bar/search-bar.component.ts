48fe9efbbca264b9368df3eca35b9b54bddfdc54
SiderealStorm <112557944+SiderealStorm@users.noreply.github.com>
2023-02-02 15:50:31 +0100

    displaying list using routes

--- IDKDO/src/app/search-bar/search-bar.component.ts
+++ IDKDO/src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SearchInput } from '../models/search-input.model';
 import { OpenAiService } from '../open-ai.service';
 import { PresentService } from '../services/present.service';
@@ -15,7 +16,7 @@ export class SearchBarComponent implements OnInit {
   @Input() input !: SearchInput;
   data !: string;
 
-  constructor(private fb : FormBuilder, private openAi : OpenAiService, private service : PresentService) {}
+  constructor(private fb : FormBuilder, private openAi : OpenAiService, private service : PresentService, private route : Router) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -42,6 +43,7 @@ export class SearchBarComponent implements OnInit {
       interest3 : data["interest3"]
     };
     this.openAi.getDataFromOpenAI(this.generateQuery())
+
   }
 
   generateQuery(): string {
