bc7ee43e86a7cbaebbefb4afd332f73b5084c5af
SiderealStorm <112557944+SiderealStorm@users.noreply.github.com>
2023-02-06 11:01:53 +0100

    adding favouites works but no picture

--- IDKDO/src/app/favourites/favourites.component.ts
+++ IDKDO/src/app/favourites/favourites.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Present } from '../models/present.model';
+import { PresentIdeaComponent } from '../present-idea/present-idea.component';
+import { PresentService } from '../services/present.service';
 
 @Component({
   selector: 'app-favourites',
@@ -10,10 +12,24 @@ import { Present } from '../models/present.model';
 export class FavouritesComponent {
 
   @Input() favList !: Present[];
+  @Input() present !: Present;
 
-  constructor(private http : HttpClient) {}
+  constructor(private http : HttpClient, private service : PresentService) {}
 
   ngOnInit() {
+    this.http.get<Present[]>('http://localhost:3000/data').subscribe(result => {
+      this.favList = result
+    })
+  }
+
+  addFavs() {
+    this.service.addFavourite(this.present).subscribe(data => {
+      console.log(data);
+      this.refreshFavs();
+    })
+  }
+
+  refreshFavs() {
     this.http.get<Present[]>('http://localhost:3000/data').subscribe(result => {
       this.favList = result
     })
--- IDKDO/src/app/services/present.service.ts
+++ IDKDO/src/app/services/present.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable, Input } from "@angular/core";
+import { Observable } from "rxjs";
 import { Present } from "../models/present.model";
 
 @Injectable ({
@@ -6,5 +8,14 @@ import { Present } from "../models/present.model";
 })
 export class PresentService {
 
+    jsonUrl: string = "http://localhost:3000/data";
 
+    constructor(private http : HttpClient) {}
+
+    addFavourite(presentIdea : Present) : Observable<any> {
+        const headers = {'content-type': 'application/json'};
+        const body = JSON.stringify(presentIdea);
+        console.log(body);
+        return this.http.post(this.jsonUrl, body, {'headers':headers});
+    }
 }
