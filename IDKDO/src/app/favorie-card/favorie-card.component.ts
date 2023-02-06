54efcff4320153c973bf03001a312dc57200dc8a
ManuelVali77 <manuelv@outlook.fr>
2023-02-06 15:27:52 +0100

    delete

--- IDKDO/src/app/favorie-card/favorie-card.component.ts
+++ IDKDO/src/app/favorie-card/favorie-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Present } from '../models/present.model';
 import { PresentService } from '../services/present.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-favorie-card',
@@ -10,10 +11,12 @@ import { PresentService } from '../services/present.service';
 export class FavorieCardComponent {
 
   @Input() presentFavorie !: Present;
+  status !: string;
 
-  constructor(private service : PresentService) {}
+  constructor(private service : PresentService, private http : HttpClient) {}
 
   deletePresent() : void {
-    this.service.removeFavourite(this.presentFavorie);
+    this.http.delete('http://localhost:3000/data/'+this.presentFavorie.id)
+    .subscribe(() => this.status = 'Delete successful');
   }
 }
